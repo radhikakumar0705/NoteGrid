5b637ab18468433fcb9fc82031c8604c3568b462
radhikakumar0705 <radhikakumar0705@gmail.com>
2025-09-03 00:15:44 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,10 +11,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
+if(process.env.NODE_ENV != "production"){
 app.use(cors({
     origin: "http://localhost:5173",
 }));
+}
 app.use(express.json()); 
 app.use(rateLimiter);
 
@@ -24,6 +28,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes)
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() =>{
     app.listen(5001, () =>{
